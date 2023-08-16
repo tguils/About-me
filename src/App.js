9a307386701534db00838ce58cd4a8339050d22f
tguils <tess.guilmette@gmail.com>
2023-08-16 18:04:35 -0400

    importing components to app.js. Setting up more piecesrobots and manifest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
-function App() {
-    return ;
-  }
+import React from 'react';
+import aboutme from './components/aboutme';
+import home from './components/home';
+import navbar from './components/navbar';
+import projects from './components/projects';
+import contact from './components/contact';
+
   
-  export default App;
+  export default function App() {
+    return (
+      <main>
+        <navbar />
+        <home />
+        <aboutme />
+        <projects />
+        <contact />
+      </main>
+    );
+  }

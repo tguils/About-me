714e1362bf1bafe3596c54ce6ca8f73327b28554
tguils <tess.guilmette@gmail.com>
2023-08-16 19:21:19 -0400

    configuring more tailwind css

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Images from './images';
 
-  
+
   export default function App() {
     return (
-      <main>
+      <main className="bg-background bg-cover h-screen">
         <Navbar />
         <Home />
         <Aboutme />
         <Projects />
+        <Images />
         <Contact />
       </main>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      BackgroundImage: {
+        'background': "url('./images/background.jpg')",
+      },
+      spacing: {
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      },
+    },
   },
   plugins: [],
 }

76ec6e3ed0e71c8d377229b8ad5bca50bd4aa7eb
tguils <tess.guilmette@gmail.com>
2023-08-17 19:17:30 -0400

    more bug fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import React from 'react';
 import Aboutme from './components/AboutMe';
 import Home from './components/Home';
@@ -6,13 +6,11 @@ import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import background from './images/background.jpg';
-
+import './App.css';
 
   export default function App() {
     return (
-      <main style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', 
-      backgroundRepeat: 'no-repeat', 
-      backgroundAttachment: 'fixed', }} className=" h-screen">
+      <main style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', }} className=" h-screen">
         <Navbar />
         <Home />
         <Aboutme />
--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+
 export default function AboutMe() {
     return (
+       
         <div>
             
         </div>

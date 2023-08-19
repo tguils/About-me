63ed513223b0cfde1d0bf38d7b81e658bffcf76a
tguils <tess.guilmette@gmail.com>
2023-08-19 09:31:33 -0400

    homepage building

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,12 @@ import './../App.css';
 
 export default function Home() {
     return (
-        <div>
-            
+        <div className='container-sm d-flex align-items-center justify-content-center' style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.25)', height: '700px' }}>
+            <div className='text-left' style={{fontWeight: 'bold'}}>
+                <div className='fs-1 mt-5' style={{ color: "#F0EB8D"}}>Welcome</div>
+                <p className='fs-5 mt-1 text-white'>Please enjoy my portfolio where you will find out about me and my work as a front-end developer</p>
+                <p className='fs-5 mt-1 text-white text-opacity-75'>JavaScript - React - CSS - HTML - MVC - </p>
+            </div>
         </div>
     );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import './../App.css';
 
 export default function Navbar() {
     return (
-      <div className=" top-0 shadow-md opacity-100 p-4 fixed w-full z-10" style={{ backgroundColor: "4C6793" }}>
+      <div className=" top-0 shadow-md  p-4 fixed w-full z-10" style={{ backgroundColor: "1d386b"}}>
         <div className="container d-flex justify-content-between align-items-center">
           <div className="text-gray-800 fs-1 font-weight-bold" style={{ color: "F0EB8D" }}>My Portfolio</div>
           <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Home</a>

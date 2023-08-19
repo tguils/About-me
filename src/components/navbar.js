ad41eedb0c8e69fe5aebb0e93cbd80ef2c2972de
tguils <tess.guilmette@gmail.com>
2023-08-18 21:35:57 -0400

    changing background, working on color pallet

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,13 @@ import './../App.css';
 
 export default function Navbar() {
     return (
-      <div className="bg-white top-0 shadow-md opacity-100 p-4 fixed w-full z-10" style={{ backgroundColor: "white" }}>
+      <div className=" top-0 shadow-md opacity-100 p-4 fixed w-full z-10" style={{ backgroundColor: "4C6793" }}>
         <div className="container d-flex justify-content-between align-items-center">
-          <div className="h2 text-gray-800 font-weight-bold">My Website</div>
-          <a href="#" className="text-gray-800 nav-link">Home</a>
-          <a href="#" className="text-gray-800 nav-link">About Me</a>
-          <a href="#" className="text-gray-800 nav-link">Projects</a>
-          <a href="#" className="text-gray-800 nav-link">Contact</a>
+          <div className="text-gray-800 fs-1 font-weight-bold" style={{ color: "F0EB8D" }}>My Portfolio</div>
+          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Home</a>
+          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>About Me</a>
+          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Projects</a>
+          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Contact</a>
         </div>
       </div>
     );

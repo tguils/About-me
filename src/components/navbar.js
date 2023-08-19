bb6b72ef429f7f5187246d3729d4581f9c137035
tguils <tess.guilmette@gmail.com>
2023-08-19 09:44:29 -0400

    added responsiveness to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,15 +3,31 @@ import './../App.css';
 
 export default function Navbar() {
     return (
-      <div className=" top-0 shadow-md  p-4 fixed w-full z-10" style={{ backgroundColor: "1d386b"}}>
-        <div className="container d-flex justify-content-between align-items-center">
-          <div className="text-gray-800 fs-1 font-weight-bold" style={{ color: "F0EB8D" }}>My Portfolio</div>
-          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Home</a>
-          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>About Me</a>
-          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Projects</a>
-          <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Contact</a>
+    <nav className="navbar navbar-expand-md navbar-dark top-0 shadow-md p-4 fixed-top" style={{ backgroundColor: "#1d386b" }}>
+    <div className="container">
+    <a className="navbar-brand text-gray-800 fs-1 font-weight-bold" style={{ color: "#F0EB8D" }}>My Portfolio</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+            <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Home</a>
+            </li>
+            <li className="nav-item">
+            <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>About Me</a>
+            </li>
+            <li className="nav-item">
+            <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Projects</a>
+            </li>
+            <li className="nav-item">
+            <a href="#" className="text-gray-800 fs-4 nav-link" style={{ color: "F0EB8D" }}>Contact</a>
+            </li>
+          </ul>
         </div>
-      </div>
+        </div>
+      
+      </nav>
     );
   }
             
